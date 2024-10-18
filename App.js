6badafae68036fcb2f61d475195165f4ebe1cf15
Your Name <21130312@st.hcmuaf.edu.vn>
2024-10-18 09:07:20 +0700

    register and login

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
 import React from 'react';
-// import { Image } from 'react-native';
-// import { Banner } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SubScreen from './view/client/pages/subScreen';
-import Default from './view/client/layout/default';
+import Login from "./view/screen/Login";
+import Register from "./view/screen/Register";
+import Verify from "./view/screen/Verify";
 
-// Create a Stack Navigator
 const Stack = createStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Default" component={Default} />
-        <Stack.Screen name="Sub" component={SubScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen
+                    name="Login"
+                    component={Login} // Đảm bảo đây là thành phần hợp lệ
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={Register} // Đảm bảo đây là thành phần hợp lệ
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Verify"
+                    component={Verify} // Đảm bảo đây là thành phần hợp lệ
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
