58733a77081e3ee05e28f2d1ed2d223a0f7a90c2
MinhThong551 <21130551@st.hcmuaf.edu.vn>
2024-10-20 18:13:27 +0700

    test

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-// import { Image } from 'react-native';
-// import { Banner } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Default from './view/client/layout/default';
-import DonHang from './view/client/layout/DonHang';
-import ChiTietDonHang from './view/client/layout/ChiTietDonHang';
-import User from './view/client/layout/User';
-// Create a Stack Navigator
-const Stack = createStackNavigator();
+import routes from './routes';
+import { BrowserRouter as Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
-            <Stack.Screen name ="DonHang" component={DonHang}/>
-             <Stack.Screen name ="User" component={User}/>
-          <Stack.Screen name ="ChiTietDonHang" component={ChiTietDonHang}/>
-        <Stack.Screen name="Default" component={Default} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Router>
+      <Routes>
+        {routes.children.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element}/>
+        ))}
+      </Routes>
+    </Router>
+
   );
 }
+
+
 
 export default App;
