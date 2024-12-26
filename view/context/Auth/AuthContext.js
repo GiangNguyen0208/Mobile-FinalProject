d5cd735608352c435deea7e89c8a1cb6add241d7
MinhThong551 <21130551@st.hcmuaf.edu.vn>
2024-12-26 15:29:54 +0700

    fix css for login add logout for user profile

--- view/context/Auth/AuthContext.js
+++ view/context/Auth/AuthContext.js
@@ -1,6 +1,5 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useNavigation } from '@react-navigation/native'; // Thêm useNavigation
 
 // Tạo context
 export const AuthContext = createContext();
@@ -15,19 +14,19 @@ export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState(null);
   const [isPrivate, setIsPrivate] = useState(true);
-  const navigation = useNavigation(); // Khởi tạo navigation
 
   useEffect(() => {
+    // Kiểm tra trạng thái đăng nhập khi ứng dụng khởi động
     const checkLoginStatus = async () => {
       try {
         const savedToken = await AsyncStorage.getItem("token");
         if (savedToken) {
-          setToken(savedToken);
-          setIsLoggedIn(true);
-          setIsPrivate(false);
+          setToken(savedToken); // Lưu token nếu đã tồn tại trong AsyncStorage
+          setIsLoggedIn(true); // Đặt trạng thái đăng nhập thành true
+          setIsPrivate(false); // Đặt quyền truy cập thành không riêng tư
         } else {
-          setIsLoggedIn(false);
-          setIsPrivate(true);
+          setIsLoggedIn(false); // Nếu không có token, đăng xuất
+          setIsPrivate(true); // Quyền truy cập là riêng tư
         }
       } catch (error) {
         console.error("Error reading token from AsyncStorage:", error);
@@ -40,22 +39,28 @@ export const AuthProvider = ({ children }) => {
   // Hàm đăng nhập
   const login = async (data) => {
     try {
+      // Trích xuất token từ phản hồi API
       const newToken = data.result.token;
+
+      // Lưu token vào AsyncStorage
       await AsyncStorage.setItem("token", newToken);
+
+      // Cập nhật state
       setToken(newToken);
-      setIsLoggedIn(true);
-      setIsPrivate(false);
+      setIsLoggedIn(true); // Đặt trạng thái đăng nhập thành true
+      setIsPrivate(false); // Quyền truy cập thành không riêng tư
     } catch (error) {
       console.error("Error saving token to AsyncStorage:", error);
     }
   };
 
-  // Hàm đăng xuất và điều hướng đến trang Login
+  // Hàm đăng xuất
   const logout = async () => {
     try {
+      // Xóa token khỏi AsyncStorage
       await AsyncStorage.removeItem("token");
-      setIsLoggedIn(false);
-      setIsPrivate(true);
+      setIsLoggedIn(false); // Đặt trạng thái đăng nhập thành false
+      setIsPrivate(true); // Đặt quyền truy cập thành riêng tư
     } catch (error) {
       console.error("Error removing token from AsyncStorage:", error);
     }
