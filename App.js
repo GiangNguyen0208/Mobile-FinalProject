fbf838abd76405fd9a6a858a843e3e2c6542bf52
Your Name <21130312@st.hcmuaf.edu.vn>
2024-12-26 14:23:03 +0700

    save code

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { NativeRouter } from 'react-router-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AllRoutes } from './view/client/components/AllRoutes'; 
+import User from './view/client/pages/Info/User';
 
 export default function App() {
-  return (
-
-    <SafeAreaProvider> 
-      <NativeRouter>
-        <NavigationContainer>
-          <AllRoutes /> 
-        </NavigationContainer>
-      </NativeRouter>
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider>
+            <User />
+        </SafeAreaProvider>
+    );
 }
