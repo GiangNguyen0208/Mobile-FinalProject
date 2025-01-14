9a954ad5a4fb61812225d3c48ca2e7614c3bae76
MinhThong551 <21130551@st.hcmuaf.edu.vn>
2025-01-14 18:21:55 +0700

    add loadingScreen before login

--- App.js
+++ App.js
@@ -4,7 +4,7 @@
 import React, { useContext, useEffect } from 'react';
 import { NativeRouter } from 'react-router-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import LoadingScreen from "./loading";
 import { AuthProvider, AuthContext } from './view/context/Auth/AuthContext'; // Đảm bảo AuthProvider và AuthContext được import đúng
 import AppNavigator from './routes';
 
@@ -25,6 +25,22 @@ function MainApp() {
 //   useEffect(() => {
 //     resetAuth();
 //   }, [resetAuth]);
+
+ const [isLoading, setIsLoading] = React.useState(true);
+
+  useEffect(() => {
+    // Mô phỏng thời gian load (ví dụ 2 giây)
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Nếu đang load, hiển thị màn hình Loading
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
 
   console.log("isLoggedIn:", isLoggedIn, "role:", role);
 
